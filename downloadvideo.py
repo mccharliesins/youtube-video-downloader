14961830f2afea3c66ffec3a20d7daf0bdda448e
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-06-02 00:22:49 +0530

    style added

--- downloadvideo.py
+++ downloadvideo.py
@@ -12,4 +12,4 @@ try:
     yt = YouTube(video_url)
 
     # Get the best quality stream for the preferred resolution
-    stream = yt.streams.filter(progressive=True, file_extens
+    stream = yt.streams.filter(progressive=True, file_extension='mp4', resolutio

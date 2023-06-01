e8502736598d75b42f45ea8c0e2951b321ef6d54
Venkateshwar Yadav (McCharlie Sins) <vkyd1909@gmail.com>
2023-06-02 00:18:49 +0530

    testing

--- downloadvideo.py
+++ downloadvideo.py
@@ -4,4 +4,5 @@ from pytube.exceptions import *
 # URL of the YouTube video you want to download
 video_url = input('Enter the YouTube video url/link: ')
 
-# Resolution preference for download (e.g., "1080p", "720p", "48
+# Resolution preference for download (e.g., "1080p", "720p", "480p", "360p", etc.)
+p
